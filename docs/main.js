90c568482f7c564b25e9b3752a5dde140ae4074e
LizardoB <jorge_lizardo26_12@hotmail.com>
2020-07-20 12:46:52 -0600

    Add files via upload

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,10 @@
 document.querySelector('.btn-menu').addEventListener('click', () => {
 	document.querySelector('.menu-links').classList.toggle('show');
 })
+
+ScrollReveal().reveal('.banner', { delay: 300 });
+ScrollReveal().reveal('.news', { delay: 300 });
+ScrollReveal().reveal('.banner1', { delay: 300 });
+ScrollReveal().reveal('.social-networks', { delay: 300 });
+
+ScrollReveal().reveal('.inicio', { delay: 300 });
